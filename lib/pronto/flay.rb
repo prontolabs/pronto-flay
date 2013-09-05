e716c6451b36a4f44ab59c2f0f52f484e8181366
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2013-09-05 22:06:15 +0300

    Naive, half-finished implementation of flay runner

--- lib/pronto/flay.rb
+++ lib/pronto/flay.rb
@@ -10,12 +10,52 @@ module Pronto
     def run(patches)
       return [] unless patches
 
-      files = diffs.select { |diff| diff.added.any? }
-                   .select { |diff| File.extname(diff.b_path) == '.rb' }
-                   .map { |diff| diff.b_blob.create_tempfile }
+      ruby_patches = patches.select do |patch|
+        path = patch.delta.new_file_full_path
+        patch.additions > 0 && File.extname(path) == '.rb'
+      end
 
-      @flay.process(*files)
-      @flay.report
+      files = ruby_patches.map do |patch|
+        File.new(patch.delta.new_file_full_path)
+      end
+
+      if files.any?
+        @flay.process(*files)
+        @flay.analyze
+        masses = Array(@flay.masses)
+
+        messages_from(masses, ruby_patches)
+      else
+        []
+      end
+    end
+
+    def messages_from(masses, ruby_patches)
+      masses.map do |mass|
+        hash = mass.first
+        nodes = @flay.hashes[hash]
+
+        patch = patch_for_node(ruby_patches, nodes.first)
+
+        line = patch.added_lines.select do |added_line|
+          added_line.new_lineno == nodes.first.line
+        end.first
+        next unless line
+        same = @flay.identical[hash]
+        level = same ? :error : :warning
+        message = 'temp'
+        Pronto::Message.new(path, line, level, message)
+      end
+    end
+
+    def patch_for_node(ruby_patches, node)
+      repo = ruby_patches.first.delta.repo
+      repo_path = Pathname.new(repo.path).parent
+      path = Pathname.new(node.file.path).relative_path_from(repo_path)
+
+      ruby_patches.select do |patch|
+        patch.delta.new_file[:path] == path.to_s
+      end.first
     end
   end
 end
